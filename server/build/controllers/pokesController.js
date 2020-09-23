34ae69c84bbbed455e7e6e840139f637bd36bfc4
Danilo Marino Bata <dmarinobata@gmail.com>
2020-09-23 15:15:14 -0300

    Actualización del front

--- server/build/controllers/pokesController.js
+++ server/build/controllers/pokesController.js
@@ -6,24 +6,51 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.pokesController = void 0;
 const database_1 = __importDefault(require("../database"));
 class PokesController {
+    constructor() {
+        this.getById = function (id, callback) {
+            database_1.default.query('SELECT * FROM poke WHERE id = ?', [id], callback);
+        };
+        this.create = function (req, res) {
+            //console.log(JSON.stringify(req.body));
+            let resultado = this.getById(req.body.id, (err, result, fields) => {
+                console.log("Actualizando");
+                console.log(result);
+                if (err)
+                    throw err;
+                if (result.length === 1) {
+                    database_1.default.query('UPDATE poke SET descr = ? WHERE id = ?', [req.body.desc, req.body.id], function (error, results, fields) {
+                        if (error) {
+                            console.log("Error updating" + error);
+                            res.json({
+                                message: "error",
+                                error: error
+                            });
+                        }
+                        else {
+                            res.json({ message: 'ok' });
+                        }
+                    });
+                }
+                else {
+                    database_1.default.query('INSERT INTO poke (id, descr) VALUES (?,?)', [req.body.id, req.body.desc], function (error, results, fields) {
+                        if (error) {
+                            console.log("Error creating connection: " + error);
+                            res.json({
+                                message: "error",
+                                error: error
+                            });
+                        }
+                        else {
+                            res.json({ message: 'ok' });
+                        }
+                    });
+                }
+            });
+        };
+    }
     index(req, res) {
         database_1.default.query('SELECT * FROM poke', (err, result, fields) => {
             res.json(result);
-        });
-    }
-    create(req, res) {
-        console.log(JSON.stringify(req.body));
-        database_1.default.query('INSERT INTO poke (id, descr) VALUES (?,?)', [req.body.id, req.body.desc], function (error, results, fields) {
-            if (error) {
-                console.log("Error creating connection: " + error);
-                res.json({
-                    message: "error",
-                    error: error
-                });
-            }
-            else {
-                res.json({ message: 'ok' });
-            }
         });
     }
     delete(req, res) {
--- server/src/controllers/pokesController.ts
+++ server/src/controllers/pokesController.ts
@@ -1,5 +1,6 @@
 import {Request, Response} from 'express';
 import pool from '../database';
+import { MysqlError } from 'mysql';
 
 class PokesController {
 
@@ -9,19 +10,43 @@ class PokesController {
         });
     }
 
+    public getById(id: string, callback:(err: MysqlError | null, result: any, fields: any) => any ){
+        pool.query('SELECT * FROM poke WHERE id = ?', [id], callback);
+    }
+
     public create(req: Request, res: Response){
-        console.log(JSON.stringify(req.body));
-        pool.query('INSERT INTO poke (id, descr) VALUES (?,?)', [req.body.id, req.body.desc], function (error, results, fields){
-            if (error) {
-                console.log("Error creating connection: " + error);
-                res.json({
-                    message: "error",
-                    error: error
-                })
-            } else {
-                res.json({message: 'ok'});
+        //console.log(JSON.stringify(req.body));
+        let resultado = this.getById(req.body.id, (err, result, fields) => {
+            console.log("Actualizando");
+            console.log(result);
+            if(err) throw err;
+            if(result.length === 1){
+                pool.query('UPDATE poke SET descr = ? WHERE id = ?', [req.body.desc, req.body.id], function (error, results, fields){
+                    if (error) {
+                        console.log("Error updating" + error);
+                        res.json({
+                            message: "error",
+                            error: error
+                        })
+                    } else {
+                        res.json({message: 'ok'});
+                    } 
+                });
+            }else{
+                pool.query('INSERT INTO poke (id, descr) VALUES (?,?)', [req.body.id, req.body.desc], function (error, results, fields){
+                    if (error) {
+                        console.log("Error creating connection: " + error);
+                        res.json({
+                            message: "error",
+                            error: error
+                        })
+                    } else {
+                        res.json({message: 'ok'});
+                    }
+                });
             }
         });
+    
     }
 
     public delete(req: Request, res: Response){
